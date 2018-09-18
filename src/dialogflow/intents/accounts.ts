eeafd3621d636d7aff0bd4cead5e7a664177f5dd
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-09-18 17:03:30 +0200

    aprovement

--- src/dialogflow/intents/accounts.ts
+++ src/dialogflow/intents/accounts.ts
@@ -82,7 +82,7 @@ export class AccountIntents /*extends BaseIntent*/ {
                 const iban4Numbers = account.iban.charAt(account.iban.length - 2) + account.iban.charAt(account.iban.length - 1) + account.iban.charAt(account.iban.length) + account.iban.charAt(account.iban.length + 1)
                 // if (iban4Numbers === last4numbers) {
                 conv.ask(iban4Numbers);
-                    conv.ask('El saldo  de la ' + account.descripcion + ' es de ' + account.balance);
+                    // conv.ask('El saldo  de la ' + account.descripcion + ' es de ' + account.balance);
                 // }
             });
         });
