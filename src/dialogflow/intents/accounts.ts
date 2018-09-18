7d359d5de21d6a2d31460439e82778a06bec9bf7
pturalgi <pturalgi@everis.com>
2018-09-18 13:05:24 +0200

    conflicts solved

--- src/dialogflow/intents/accounts.ts
+++ src/dialogflow/intents/accounts.ts
@@ -15,43 +15,43 @@ export class AccountIntents /*extends BaseIntent*/ {
         //Lista cuentas
         app.intent('Cuentas', (conv) => {
             conv.ask('blablabla' + accounts.length);
-            if (accounts.length > 1) {
-                var voice = 'Tus cuentas son' + ' ';
-                const tmp = {
-                    title: 'Mis Cuentas' + ' ',
-                    items: {}
-                };
-                accounts.forEach((account) => {
-                    voice = voice + ' ' + account.description + ',';
-                    tmp.items[account.iban] = {
-                        title: account.description,
-                        description: account.iban,
-                        image: {
-                            url: accountImage,
-                            accessibilityText: account.description
-                        }
-                    };
-                });
-                conv.ask(new List(tmp));
-                conv.ask(voice);
-                conv.ask('Puedes preguntame por el saldo o los movimientos de una cuenta');
-            } else {
-                conv.ask('Dispones de la cuenta ' + accounts[0].description + ' ');
-                conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
-                conv.ask(new BasicCard({
-                    title: 'Abrir App',
-                    image: {
-                        url: accountImage,
-                        accessibilityText: 'Abrir APP'
-                    },
-                    text: '',
-                    buttons: new Button({
-                        title: 'Abrir APP',
-                        url: 'http://eduvecino.com/GA_BMA/app_saba.php',
-                    })
-                })
-                );
-            }
+            // if (accounts.length > 1) {
+            //     var voice = 'Tus cuentas son' + ' ';
+            //     const tmp = {
+            //         title: 'Mis Cuentas' + ' ',
+            //         items: {}
+            //     };
+            //     accounts.forEach((account) => {
+            //         voice = voice + ' ' + account.description + ',';
+            //         tmp.items[account.iban] = {
+            //             title: account.description,
+            //             description: account.iban,
+            //             image: {
+            //                 url: accountImage,
+            //                 accessibilityText: account.description
+            //             }
+            //         };
+            //     });
+            //     conv.ask(new List(tmp));
+            //     conv.ask(voice);
+            //     conv.ask('Puedes preguntame por el saldo o los movimientos de una cuenta');
+            // } else {
+            //     conv.ask('Dispones de la cuenta ' + accounts[0].description + ' ');
+            //     conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
+            //     conv.ask(new BasicCard({
+            //         title: 'Abrir App',
+            //         image: {
+            //             url: accountImage,
+            //             accessibilityText: 'Abrir APP'
+            //         },
+            //         text: '',
+            //         buttons: new Button({
+            //             title: 'Abrir APP',
+            //             url: 'http://eduvecino.com/GA_BMA/app_saba.php',
+            //         })
+            //     })
+            //     );
+            // }
         });
 
         //Detalle cuenta seleccionada
