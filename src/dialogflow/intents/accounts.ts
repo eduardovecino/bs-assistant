370ea19d52e40310477153ad5e4e1660e37d797e
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-09-18 17:14:18 +0200

    aprovement

--- src/dialogflow/intents/accounts.ts
+++ src/dialogflow/intents/accounts.ts
@@ -80,10 +80,10 @@ export class AccountIntents /*extends BaseIntent*/ {
         app.intent('Saldo cuenta', (conv, { last4numbers }) => {
             accounts.forEach((account) => {
                 const iban4Numbers = account.iban.charAt(account.iban.length - 2)+account.iban.charAt(account.iban.length - 1)+account.iban.charAt(account.iban.length)+account.iban.charAt(account.iban.length + 1)
-                if (parseInt(iban4Numbers) === last4numbers) {
-                conv.ask(iban4Numbers);
+                // if (parseInt(iban4Numbers) === last4numbers) {
+                conv.ask(parseInt(last4numbers), (parseInt(iban4Numbers)));
                     // conv.ask('El saldo  de la ' + account.descripcion + ' es de ' + account.balance);
-                 }
+                //  }
             });
         });
     }
