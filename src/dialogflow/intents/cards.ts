96657ff0143063591b96d8591ac7f3c8bce5dbe3
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-09-19 10:46:34 +0200

    saldo tarjeta

--- src/dialogflow/intents/cards.ts
+++ src/dialogflow/intents/cards.ts
@@ -93,8 +93,8 @@ export class CardIntents /*extends BaseIntent*/ {
                     if (parseInt(last4CardNumbers) === parseInt(card4Numbers) /*|| tipo_tarjeta === cards.--- */) {
                         encontrada = 1;
                         conv.ask('El saldo  de la ' + card.cuentaRelacionada + ' es de ' + card.saldoDisponible);
-                    } else if (encontrada = 0 && cards.length + 1 === index) {
-                        conv.ask(encontrada + cards.length +1 + index)
+                    } else /*if (encontrada = 0 && cards.length + 1 === index)*/ {
+                        conv.ask(index)
                         // conv.ask('No se ha encontrado ninguna tarjeta, prueba en decir el tipo de cuenta o los 4 últimos numeros');
                     }
                 });
