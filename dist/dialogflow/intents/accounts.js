9b33db26cc72426f69f2b7d4a03561a6ce901608
pturalgi <pturalgi@everis.com>
2018-09-19 08:08:18 +0200

    iban

--- dist/dialogflow/intents/accounts.js
+++ dist/dialogflow/intents/accounts.js
@@ -76,10 +76,8 @@ class AccountIntents /*extends BaseIntent*/ {
                 // const iban4Numbers = account.iban.charAt(account.iban.length - 3)+account.iban.charAt(account.iban.length -2)+account.iban.charAt(account.iban.length-1)+account.iban.charAt(account.iban.length)
                 const iban4Numbers = account.iban.charAt(account.iban.length - 4) + account.iban.charAt(account.iban.length - 3) + account.iban.charAt(account.iban.length - 2) + account.iban.charAt(account.iban.length - 1);
                 if (last4numbers === iban4Numbers) {
-                    // conv.ask(iban4Numbers + ' ' + last4numbers);
                     conv.ask(iban4Numbers + last4numbers);
                     // conv.ask('El saldo  de la ' + account.descripcion + ' es de ' + account.balance);
-                    //  }
                 }
             });
         });
