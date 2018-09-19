5029c8eb7afcb17e38fc5cb3bb473c707d790622
pturalgi <pturalgi@everis.com>
2018-09-19 11:02:20 +0200

    accounts intents

--- dist/dialogflow/intents/accounts.js
+++ dist/dialogflow/intents/accounts.js
@@ -7,7 +7,7 @@ class AccountIntents /*extends BaseIntent*/ {
         this.accountService = new account_service_1.AccountService();
     }
     intents(app) {
-        const accountImage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Recibo_de_pago_-_modelo_simple.svg/300px-Recibo_de_pago_-_modelo_simple.svg.png';
+        const accountImage = 'https://es.banqueando.com/wp-content/uploads/2012/05/logotipo_sabadell_creditos_banco11.gif';
         const accounts = this.accountService.getAccounts();
         //Lista cuentas
         app.intent('Cuentas', (conv) => {
@@ -33,20 +33,7 @@ class AccountIntents /*extends BaseIntent*/ {
                 conv.ask('Puedes preguntame por el saldo o los movimientos de una cuenta');
             }
             else {
-                conv.ask('Dispones de la cuenta ' + accounts[0].descripcion + ' ');
-                conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
-                conv.ask(new actions_on_google_1.BasicCard({
-                    title: 'Abrir App',
-                    image: {
-                        url: accountImage,
-                        accessibilityText: 'Abrir APP'
-                    },
-                    text: '',
-                    buttons: new actions_on_google_1.Button({
-                        title: 'Abrir APP',
-                        url: 'http://eduvecino.com/GA_BMA/app_saba.php',
-                    })
-                }));
+                conv.ask('El saldo  de tu ' + accounts[0].descripcion + ' es de ' + accounts[0].balance);
             }
         });
         //Detalle cuenta seleccionada
@@ -54,32 +41,45 @@ class AccountIntents /*extends BaseIntent*/ {
             accounts.forEach((account) => {
                 if (account.iban === option) {
                     conv.ask('Has seleccionado la cuenta ' + account.descripcion + ' ');
-                    conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
-                    conv.ask(new actions_on_google_1.BasicCard({
-                        title: 'Abrir App',
-                        image: {
-                            url: accountImage,
-                            accessibilityText: 'Abrir APP'
-                        },
-                        text: '',
-                        buttons: new actions_on_google_1.Button({
-                            title: 'Abrir APP',
-                            url: 'http://eduvecino.com/GA_BMA/app_saba.php',
-                        })
-                    }));
+                    conv.ask('El saldo es de ' + accounts[0].balance);
                 }
             });
+            // accounts.forEach((account) => {
+            //     if (account.iban === option) {
+            //         conv.ask('Has seleccionado la cuenta ' + account.descripcion + ' ');
+            //         conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
+            //         conv.ask(new BasicCard({
+            //             title: 'Abrir App',
+            //             image: {
+            //                 url: accountImage,
+            //                 accessibilityText: 'Abrir APP'
+            //             },
+            //             text: '',
+            //             buttons: new Button({
+            //                 title: 'Abrir APP',
+            //                 url: 'http://eduvecino.com/GA_BMA/app_saba.php',
+            //             })
+            //         })
+            //         );
+            //     }
+            // });
         });
         // Saldo cuenta
-        app.intent('Saldo cuenta', (conv, { last4numbers }) => {
-            accounts.forEach((account) => {
-                // const iban4Numbers = account.iban.charAt(account.iban.length - 3)+account.iban.charAt(account.iban.length -2)+account.iban.charAt(account.iban.length-1)+account.iban.charAt(account.iban.length)
-                const iban4Numbers = account.iban.charAt(account.iban.length - 4) + account.iban.charAt(account.iban.length - 3) + account.iban.charAt(account.iban.length - 2) + account.iban.charAt(account.iban.length - 1);
-                if (parseInt(last4numbers) === parseInt(iban4Numbers)) {
-                    // conv.ask(iban4Numbers + last4numbers);
-                    conv.ask('El saldo  de la ' + account.descripcion + ' es de ' + account.balance);
-                }
-            });
+        app.intent('Saldo cuenta', (conv, { last4numbers }, { tipo_cuenta }) => {
+            if (accounts.length === 1) {
+                conv.ask('El saldo  de tu ' + accounts[0].descripcion + ' es de ' + accounts[0].balance);
+            }
+            else {
+                accounts.forEach((account) => {
+                    const iban4Numbers = account.iban.charAt(account.iban.length - 4) + account.iban.charAt(account.iban.length - 3) + account.iban.charAt(account.iban.length - 2) + account.iban.charAt(account.iban.length - 1);
+                    if (parseInt(last4numbers) === parseInt(iban4Numbers) || tipo_cuenta === account.descripcion) {
+                        conv.ask('El saldo  de tu ' + account.descripcion + ' es de ' + account.balance);
+                    }
+                    else {
+                        conv.ask('No se ha encontrado ninguna cuenta, prueba en decir el tipo de cuenta o los 4 últimos numeros');
+                    }
+                });
+            }
         });
     }
 }
--- src/dialogflow/intents/accounts.ts
+++ src/dialogflow/intents/accounts.ts
@@ -9,7 +9,7 @@ export class AccountIntents /*extends BaseIntent*/ {
 
     public intents(app): void {
 
-        const accountImage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Recibo_de_pago_-_modelo_simple.svg/300px-Recibo_de_pago_-_modelo_simple.svg.png';
+        const accountImage = 'https://es.banqueando.com/wp-content/uploads/2012/05/logotipo_sabadell_creditos_banco11.gif';
         const accounts = this.accountService.getAccounts();
 
         //Lista cuentas
@@ -35,21 +35,7 @@ export class AccountIntents /*extends BaseIntent*/ {
                 conv.ask(voice);
                 conv.ask('Puedes preguntame por el saldo o los movimientos de una cuenta');
             } else {
-                conv.ask('Dispones de la cuenta ' + accounts[0].descripcion + ' ');
-                conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
-                conv.ask(new BasicCard({
-                    title: 'Abrir App',
-                    image: {
-                        url: accountImage,
-                        accessibilityText: 'Abrir APP'
-                    },
-                    text: '',
-                    buttons: new Button({
-                        title: 'Abrir APP',
-                        url: 'http://eduvecino.com/GA_BMA/app_saba.php',
-                    })
-                })
-                );
+                conv.ask('El saldo  de tu ' + accounts[0].descripcion + ' es de ' + accounts[0].balance);
             }
         });
 
@@ -58,34 +44,45 @@ export class AccountIntents /*extends BaseIntent*/ {
             accounts.forEach((account) => {
                 if (account.iban === option) {
                     conv.ask('Has seleccionado la cuenta ' + account.descripcion + ' ');
-                    conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
-                    conv.ask(new BasicCard({
-                        title: 'Abrir App',
-                        image: {
-                            url: accountImage,
-                            accessibilityText: 'Abrir APP'
-                        },
-                        text: '',
-                        buttons: new Button({
-                            title: 'Abrir APP',
-                            url: 'http://eduvecino.com/GA_BMA/app_saba.php',
-                        })
-                    })
-                    );
+                    conv.ask('El saldo es de ' + accounts[0].balance);
                 }
             });
+            
+            // accounts.forEach((account) => {
+            //     if (account.iban === option) {
+            //         conv.ask('Has seleccionado la cuenta ' + account.descripcion + ' ');
+            //         conv.ask('Puedes obtener el saldo de la cuenta o el listado de movimientos');
+            //         conv.ask(new BasicCard({
+            //             title: 'Abrir App',
+            //             image: {
+            //                 url: accountImage,
+            //                 accessibilityText: 'Abrir APP'
+            //             },
+            //             text: '',
+            //             buttons: new Button({
+            //                 title: 'Abrir APP',
+            //                 url: 'http://eduvecino.com/GA_BMA/app_saba.php',
+            //             })
+            //         })
+            //         );
+            //     }
+            // });
         });
 
         // Saldo cuenta
-        app.intent('Saldo cuenta', (conv, { last4numbers }) => {
-            accounts.forEach((account) => {
-                // const iban4Numbers = account.iban.charAt(account.iban.length - 3)+account.iban.charAt(account.iban.length -2)+account.iban.charAt(account.iban.length-1)+account.iban.charAt(account.iban.length)
-                const iban4Numbers = account.iban.charAt(account.iban.length - 4) + account.iban.charAt(account.iban.length - 3) + account.iban.charAt(account.iban.length - 2) + account.iban.charAt(account.iban.length-1);
-                if (parseInt(last4numbers) === parseInt(iban4Numbers)) {
-                // conv.ask(iban4Numbers + last4numbers);
-                    conv.ask('El saldo  de la ' + account.descripcion + ' es de ' + account.balance);
-                 }
-            });
+        app.intent('Saldo cuenta', (conv, { last4numbers }, { tipo_cuenta }) => {
+            if (accounts.length === 1) {
+                conv.ask('El saldo  de tu ' + accounts[0].descripcion + ' es de ' + accounts[0].balance);
+            } else {
+                accounts.forEach((account) => {
+                    const iban4Numbers = account.iban.charAt(account.iban.length - 4) + account.iban.charAt(account.iban.length - 3) + account.iban.charAt(account.iban.length - 2) + account.iban.charAt(account.iban.length - 1);
+                    if (parseInt(last4numbers) === parseInt(iban4Numbers) || tipo_cuenta === account.descripcion) {
+                        conv.ask('El saldo  de tu ' + account.descripcion + ' es de ' + account.balance);
+                    } else {
+                        conv.ask('No se ha encontrado ninguna cuenta, prueba en decir el tipo de cuenta o los 4 últimos numeros');
+                    }
+                });
+            }
         });
     }
 }
