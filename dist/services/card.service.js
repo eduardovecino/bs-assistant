fe5e4903be36ed00a4685d39a507c53a369ac482
pturalgi <pturalgi@everis.com>
2018-09-20 08:28:07 +0200

    changes (for)

--- dist/services/card.service.js
+++ dist/services/card.service.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rest_manager_1 = require("../managers/rest.manager");
+const card_manager_1 = require("../managers/card.manager");
 const fs = require("fs");
 class CardService extends rest_manager_1.RestManager {
     getCards() {
@@ -12,9 +13,10 @@ class CardService extends rest_manager_1.RestManager {
     }
     getCard(last4) {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/card/get-card.json');
+            const data = fs.readFileSync('mock/card/get-cards.json');
             const jsonData = JSON.parse(data.toString());
-            resolve(jsonData.data);
+            const card = card_manager_1.CardManager.getCardByLast4(jsonData.data, last4);
+            resolve(card);
         });
     }
 }
--- src/services/card.service.ts
+++ src/services/card.service.ts
@@ -1,4 +1,6 @@
 import { RestManager } from "../managers/rest.manager";
+import { CardManager } from "../managers/card.manager";
+
 import * as fs from "fs";
 
 export class CardService extends RestManager {
@@ -13,9 +15,10 @@ export class CardService extends RestManager {
 
     public getCard(last4): Promise<any> {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/card/get-card.json');
+            const data = fs.readFileSync('mock/card/get-cards.json');
             const jsonData = JSON.parse(data.toString());
-            resolve(jsonData.data);
+            const card = CardManager.getCardByLast4(jsonData.data, last4);
+            resolve(card);
         });
     }
 }
