93a62ec243a68590c7975967f37b22f156d13570
pturalgi <pturalgi@everis.com>
2018-09-21 09:28:33 +0200

    offices intent

--- dist/dialogflow/intents/info.js
+++ dist/dialogflow/intents/info.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const information_service_1 = require("../../services/information.service");
-const information_manager_1 = require("../../managers/dialog-flow/information.manager");
 class InfoIntents /*extends BaseIntent*/ {
     constructor() {
         this.informationService = new information_service_1.InformationService();
@@ -9,15 +8,14 @@ class InfoIntents /*extends BaseIntent*/ {
     intents(app) {
         app.intent('Oficinas Cercanas', conv => {
             this.informationService.getOffices().then(offices => {
-                // conv.ask(offices[0].latitude);
-                if (offices) {
-                    const carouselOfOffices = information_manager_1.InformationDFManager.generateOfficesBrowseCarousel(offices);
-                    conv.ask('Aquí tienes las oficinas más cercanas');
-                    conv.ask(carouselOfOffices);
-                }
-                else {
-                    conv.ask('No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo');
-                }
+                conv.ask(offices[0].latitude);
+                // if (offices) {
+                //     const carouselOfOffices = InformationDFManager.generateOfficesBrowseCarousel(offices);
+                //     conv.ask('Aquí tienes las oficinas más cercanas');
+                //     conv.ask(carouselOfOffices);
+                // } else {
+                //     conv.ask('No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo');
+                // }
             });
         });
     }
