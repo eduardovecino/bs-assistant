5717a40ea2a9e70dcac1a69520876a87fce41cc9
pturalgi <pturalgi@everis.com>
2018-09-21 08:28:04 +0200

    offices intent

--- src/dialogflow/intents/info.ts
+++ src/dialogflow/intents/info.ts
@@ -11,13 +11,14 @@ export class InfoIntents /*extends BaseIntent*/ {
 
         app.intent('Oficinas', conv => {
             this.informationService.getOffices().then(offices => {
-                if (offices) {
-                    const carouselOfOffices = InformationDFManager.generateOfficesBrowseCarousel(offices);
-                    conv.ask('Aquí tienes las oficinas más cercanas');
-                    conv.ask(carouselOfOffices);
-                } else {
-                    conv.ask('No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo');
-                }
+                conv.ask(offices[0].latitude);
+                // if (offices) {
+                //     const carouselOfOffices = InformationDFManager.generateOfficesBrowseCarousel(offices);
+                //     conv.ask('Aquí tienes las oficinas más cercanas');
+                //     conv.ask(carouselOfOffices);
+                // } else {
+                //     conv.ask('No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo');
+                // }
             });
         });
     }
