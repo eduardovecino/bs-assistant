921ae91797528fd03738558c2ef702a0c7953e98
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-09-25 10:53:59 +0200

    changes on carousel offices

--- src/managers/dialog-flow/information.manager.ts
+++ src/managers/dialog-flow/information.manager.ts
@@ -11,16 +11,16 @@ export class InformationDFManager {
             items: []
         };
         offices.forEach((office) => {
-            const mapUrl = `https://www.google.com/maps/place/El+Patio+de+Mi+Casa+Clot/@41.4104223,2.1907937,15z/data=!4m8!1m2!2m1!1smi+casa!3m4!1s0x12a4a32698dcb1eb:0x12cc38232c06459d!8m2!3d41.4068526!4d2.1888659`;
+            const mapUrl = `https://maps.google.com/?q=${office.latitude},${office.longitude}`;
             tmp.items.push( new BrowseCarouselItem(
                 {
                     title: office.id,
                     url: mapUrl,
                     description: office.address,
-                    image: new Image ({
-                        url: 'https://www.busconomico.com/Images/Blog/BSCard.jpg',
-                        alt: office.id
-                    })
+                    // image: new Image ({
+                    //     url: 'https://www.busconomico.com/Images/Blog/BSCard.jpg',
+                    //     alt: office.id
+                    // })
                 })
             );
         });
