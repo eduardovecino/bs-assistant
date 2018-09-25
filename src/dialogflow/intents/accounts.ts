56627f0153589b2fe0df917f74262459233c8de6
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-09-25 11:05:21 +0200

    changes on carousel offices

--- src/dialogflow/intents/accounts.ts
+++ src/dialogflow/intents/accounts.ts
@@ -2,6 +2,8 @@ import { AccountService } from "../../services/account.service";
 import { AccountManager } from "../../managers/data/account.manager";
 import { AccountDFManager } from "../../managers/dialog-flow/account.manager";
 import { SuggestionDFManager } from "../../managers/dialog-flow/suggestion.manager";
+import { Suggestions } from "actions-on-google";
+import { SUGGESTIONS } from "../../constants/suggestions";
 
 
 export class AccountIntents /*extends BaseIntent*/ {
@@ -20,7 +22,7 @@ export class AccountIntents /*extends BaseIntent*/ {
                     const accountsList = AccountDFManager.generateAccountsList(accounts);
                     conv.ask(accountsList);
                     // conv.ask(suggestionResponse);
-                    conv.ask(SuggestionDFManager.generateSuggestions())
+                    conv.ask(new Suggestions(SUGGESTIONS.NOT_LOGGED_SUGGESTIONS))
                 } else {
                     conv.ask(nullResponse);
                 }
