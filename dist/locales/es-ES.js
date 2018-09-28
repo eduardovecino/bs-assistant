d262aabb53d611e2ea86034604a91bdfd4bee9d7
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-09-28 11:32:38 +0200

    improvement json

--- dist/locales/es-ES.js
+++ dist/locales/es-ES.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const ssml_gib_1 = require("ssml-gib");
 exports.SPANISH_TRANSLATIONS = {
-    'intent.product.welcome.answer': 'You have three seconds to think about it blablaab... \"Ssml.break({ s: 3 })\" Para dirigirme a usted por su nombre y conocer su ubicación,',
+    'intent.product.welcome.answer': 'You have three seconds to think about it blablaab...' + ssml_gib_1.Ssml.break({ s: 3 }) + 'Para dirigirme a usted por su nombre y conocer su ubicación',
     'intent.product.welcome.answer_%name%': 'Bienvenido a Banco Sabadell, {{ name }}'
-}; // let ssml = [ + this.translateManager.translate('intent.product.welcome.answer')];
+};
 //# sourceMappingURL=es-ES.js.map
--- src/locales/es-ES.ts
+++ src/locales/es-ES.ts
@@ -1,4 +1,6 @@
+import { Ssml } from "ssml-gib";
+
 export const SPANISH_TRANSLATIONS = {
-    'intent.product.welcome.answer': 'You have three seconds to think about it blablaab... \"Ssml.break({ s: 3 })\" Para dirigirme a usted por su nombre y conocer su ubicación,',
+    'intent.product.welcome.answer': 'You have three seconds to think about it blablaab...' + Ssml.break({ s: 3 }) + 'Para dirigirme a usted por su nombre y conocer su ubicación',
     'intent.product.welcome.answer_%name%': 'Bienvenido a Banco Sabadell, {{ name }}'
-}            // let ssml = [ + this.translateManager.translate('intent.product.welcome.answer')];
+}
