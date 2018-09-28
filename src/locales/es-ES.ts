af8959d559b53de386f63fe1d6f465a31352cb46
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-09-28 14:02:05 +0200

    translations tests

--- src/locales/es-ES.ts
+++ src/locales/es-ES.ts
@@ -1,3 +1,5 @@
+import { Ssml } from "ssml-gib";
+
 
 export const SPANISH_TRANSLATIONS = {
     'intent.product.welcome.answer': `You have three seconds to think about it blablaab... ${ SSML.break_3 } para dirigirme a usted por su nombre y conocer su ubicación`,
