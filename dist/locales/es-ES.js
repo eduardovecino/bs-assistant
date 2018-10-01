56d4a0e0e368980acba9860d8962853f9299d9c6
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-10-01 08:40:46 +0200

    translations tests

--- dist/locales/es-ES.js
+++ dist/locales/es-ES.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const ssml_gib_1 = require("ssml-gib");
 exports.SPANISH_TRANSLATIONS = {
-    'intent.product.welcome.answer': `para dirigirme a usted por su nombre y conocer su ubicación`,
+    'intent.product.welcome.answer': 'para dirigirme a usted por su nombre y conocer su ubicación',
     'intent.product.welcome.answer_%name%': `Bienvenido a Banco Sabadell, ${ssml_gib_1.Ssml.break({ s: 2 })} {{ name }}`
 };
 //# sourceMappingURL=es-ES.js.map
--- src/locales/es-ES.ts
+++ src/locales/es-ES.ts
@@ -1,6 +1,6 @@
 import { Ssml } from "ssml-gib";
 
 export const SPANISH_TRANSLATIONS = {
-    'intent.product.welcome.answer': `para dirigirme a usted por su nombre y conocer su ubicación`,
+    'intent.product.welcome.answer': 'para dirigirme a usted por su nombre y conocer su ubicación',
     'intent.product.welcome.answer_%name%': `Bienvenido a Banco Sabadell, ${Ssml.break({ s: 2 })} {{ name }}`
 }
