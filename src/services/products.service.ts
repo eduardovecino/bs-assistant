037109b7079ca63e346d300d22d3dd253f841525
pturalgi <pturalgi@everis.com>
2018-10-02 12:59:57 +0200

    account linking test

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,6 +1,7 @@
 import { RestManager } from "../managers/data/rest.manager";
 
 import * as fs from "fs";
+import * as https from 'https';
 
 export class ProductService extends RestManager {
 
@@ -10,10 +11,22 @@ export class ProductService extends RestManager {
             const jsonData = JSON.parse(data.toString());
             return jsonData;
         } else {
-            // return this.get();
-            const data = fs.readFileSync('mock/products/get-products.json');
-            const jsonData = JSON.parse(data.toString());
-            return jsonData;
+            // const data = fs.readFileSync('mock/products/get-products.json');
+            // const jsonData = JSON.parse(data.toString());
+            // return jsonData;
+            let options = {
+                host: 'https://oauth.bancsabadell.com',
+                path: '/ResourcesServerBS/oauthservices/v1.0.0/productos',
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    authorization: 'Bearer ef87983a-ee9e-435e-8380-e482993b06342434ca7d-5d95-488b-a8ef-fc0b88bf3860f7cea5da-adf6-4dfd-a6a6-cafb3223c755',
+                }
+            }
+            https.request(options, (data)=>{
+                const jsonData = JSON.parse(data.toString());
+                return jsonData
+            })
         }
     }
 }
