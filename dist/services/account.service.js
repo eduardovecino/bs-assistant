5cf1d4d1b99605df0d5d2c6d851b37e54348ddf9
pturalgi <pturalgi@everis.com>
2018-10-02 13:05:37 +0200

    account linking test

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -1,22 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const rest_manager_1 = require("../managers/data/rest.manager");
-const account_manager_1 = require("../managers/data/account.manager");
-const fs = require("fs");
+const https = require("https");
 class AccountService extends rest_manager_1.RestManager {
     getAccounts() {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/accounts/get-accounts.json');
-            const jsonData = JSON.parse(data.toString());
-            resolve(jsonData.data);
+            // const data = fs.readFileSync('mock/accounts/get-accounts.json');
+            // const jsonData = JSON.parse(data.toString());
+            // resolve(jsonData.data);
+            let options = {
+                host: 'https://oauth.bancsabadell.com',
+                path: '/ResourcesServerBS/oauthservices/v1.0.0/productos',
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    authorization: 'Bearer ef87983a-ee9e-435e-8380-e482993b06342434ca7d-5d95-488b-a8ef-fc0b88bf3860f7cea5da-adf6-4dfd-a6a6-cafb3223c755',
+                }
+            };
+            https.request(options, (data) => {
+                const jsonData = JSON.parse(data.toString());
+                resolve(jsonData);
+            });
         });
     }
     getAccount(last4) {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/accounts/get-account.json');
-            const jsonData = JSON.parse(data.toString());
-            const card = account_manager_1.AccountManager.getAccountByLast4(jsonData.data, last4);
-            resolve(card);
+            // const data = fs.readFileSync('mock/accounts/get-account.json');
+            // const jsonData = JSON.parse(data.toString());
+            // const card = AccountManager.getAccountByLast4(jsonData.data, last4);
+            // resolve(card);
+            let options = {
+                host: 'https://oauth.bancsabadell.com',
+                path: '/ResourcesServerBS/oauthservices/v1.0.0/productos',
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    authorization: 'Bearer ef87983a-ee9e-435e-8380-e482993b06342434ca7d-5d95-488b-a8ef-fc0b88bf3860f7cea5da-adf6-4dfd-a6a6-cafb3223c755',
+                }
+            };
+            https.request(options, (data) => {
+                const jsonData = JSON.parse(data.toString());
+                resolve(jsonData);
+            });
         });
     }
 }
--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -2,23 +2,51 @@ import { RestManager } from "../managers/data/rest.manager";
 import { AccountManager } from "../managers/data/account.manager";
 
 import * as fs from "fs";
+import * as https from 'https';
 
 export class AccountService extends RestManager {
 
     public getAccounts(): Promise<any> {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/accounts/get-accounts.json');
-            const jsonData = JSON.parse(data.toString());
-            resolve(jsonData.data);
+            // const data = fs.readFileSync('mock/accounts/get-accounts.json');
+            // const jsonData = JSON.parse(data.toString());
+            // resolve(jsonData.data);
+
+            let options = {
+                host: 'https://oauth.bancsabadell.com',
+                path: '/ResourcesServerBS/oauthservices/v1.0.0/productos',
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    authorization: 'Bearer ef87983a-ee9e-435e-8380-e482993b06342434ca7d-5d95-488b-a8ef-fc0b88bf3860f7cea5da-adf6-4dfd-a6a6-cafb3223c755',
+                }
+            }
+            https.request(options, (data) => {
+                const jsonData = JSON.parse(data.toString());
+                resolve (jsonData)
+            })
         });
     }
 
     public getAccount(last4): Promise<any> {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/accounts/get-account.json');
-            const jsonData = JSON.parse(data.toString());
-            const card = AccountManager.getAccountByLast4(jsonData.data, last4);
-            resolve(card);
+            // const data = fs.readFileSync('mock/accounts/get-account.json');
+            // const jsonData = JSON.parse(data.toString());
+            // const card = AccountManager.getAccountByLast4(jsonData.data, last4);
+            // resolve(card);
+            let options = {
+                host: 'https://oauth.bancsabadell.com',
+                path: '/ResourcesServerBS/oauthservices/v1.0.0/productos',
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    authorization: 'Bearer ef87983a-ee9e-435e-8380-e482993b06342434ca7d-5d95-488b-a8ef-fc0b88bf3860f7cea5da-adf6-4dfd-a6a6-cafb3223c755',
+                }
+            }
+            https.request(options, (data) => {
+                const jsonData = JSON.parse(data.toString());
+                resolve(jsonData)
+            })
         });
     }
 }
