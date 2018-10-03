cfc40bdda3052951e351fea96b58019dd060d5f4
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-10-03 12:52:09 +0200

    changes

--- src/dialogflow/intents/cards.ts
+++ src/dialogflow/intents/cards.ts
@@ -52,7 +52,7 @@ export class CardIntents /*extends BaseIntent*/ {
 
 
             app.intent('Tarjeta seleccionada - no', (conv, input, output) => {
-                conv.close(`Nos vemos pronto ${conv.user.profile.displayName} `)
+                conv.close(`Nos vemos pronto ${conv.user.profile.displayName}`)
             }) 
 
         // //BLOQUEAR TARJETA
