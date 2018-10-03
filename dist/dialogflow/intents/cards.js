418de1d83c936d254014678e6ab3247c641d0a38
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-10-03 12:50:52 +0200

    changes

--- dist/dialogflow/intents/cards.js
+++ dist/dialogflow/intents/cards.js
@@ -47,7 +47,7 @@ class CardIntents /*extends BaseIntent*/ {
             conv.ask('Hola');
         });
         app.intent('Tarjeta seleccionada - no', (conv, input, output) => {
-            conv.close(`Nos vemos pronto ${conv.user.profile.payload.name} `);
+            conv.close(`Nos vemos pronto ${conv.user.profile.displayName} `);
         });
         // //BLOQUEAR TARJETA
         app.intent('Bloquear tarjeta', (conv) => {
