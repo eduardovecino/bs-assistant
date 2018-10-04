c7bf3cbec7710853cb44ce6057277f3c7cfe48cc
pturalgi <pturalgi@everis.com>
2018-10-04 09:17:32 +0200

    account linking

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -7,30 +7,30 @@ export class AccountService extends RestManager {
 
     public getAccounts(): Promise<any> {
         return new Promise((resolve, reject) => {
-            // const data = fs.readFileSync('mock/accounts/get-accounts.json');
-            // const jsonData = JSON.parse(data.toString());
-            // resolve(jsonData.data);
 
-            const request = require('request');
+            setTimeout(function () {
+                const data = fs.readFileSync('mock/accounts/get-accounts.json');
+                const jsonData = JSON.parse(data.toString());
+                resolve(jsonData.data);
+            }, 4000);
 
-            const options = {
-                'method': 'GET',
-                'uri': 'https://oauth.bancsabadell.com/ResourcesServerBS/oauthservices/v1.0.0/productos',
-                'json': true,
-                'headers': {
-                    'content-Type': 'application/json',
-                    // 'accept-encoding': 'gzip, deflate',
-                    'Authorization': 'Bearer 7da398b8-0b10-4b85-bb81-7e7a86cc2bfe0a8db05d-83fc-4f24-9f03-e685739592e0ac0eaa97-0356-469a-ba02-e7dd74ee81dc',
-                }
-            };
+            // const request = require('request');
 
-            request(options, (err, res, body) => {
-                if (err) { return console.log(err); }
-                resolve(body.data);
-                console.log(body.data);
-            });
+            // const options = {
+            //     'method': 'GET',
+            //     'uri': 'https://oauth.bancsabadell.com/ResourcesServerBS/oauthservices/v1.0.0/productos',
+            //     'json': true,
+            //     'headers': {
+            //         'content-Type': 'application/json',
+            //         'Authorization': 'Bearer 7da398b8-0b10-4b85-bb81-7e7a86cc2bfe0a8db05d-83fc-4f24-9f03-e685739592e0ac0eaa97-0356-469a-ba02-e7dd74ee81dc',
+            //     }
+            // };
 
-
+            // request(options, (err, res, body) => {
+            //     if (err) { return console.log(err); }
+            //     resolve(body.data);
+            //     console.log(body.data);
+            // });
         });        
     }
 
