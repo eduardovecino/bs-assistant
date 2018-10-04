593d06fb740707929df26ea58753138bd53e9462
pturalgi <pturalgi@everis.com>
2018-10-04 09:53:30 +0200

    account linking

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -6,26 +6,27 @@ const fs = require("fs");
 class AccountService extends rest_manager_1.RestManager {
     getAccounts() {
         return new Promise((resolve, reject) => {
-            setTimeout(function () {
-                const data = fs.readFileSync('mock/accounts/get-accounts.json');
-                const jsonData = JSON.parse(data.toString());
-                resolve(jsonData.data);
-            }, 100);
-            // const request = require('request');
-            // const options = {
-            //     'method': 'GET',
-            //     'uri': 'https://oauth.bancsabadell.com/ResourcesServerBS/oauthservices/v1.0.0/productos',
-            //     'json': true,
-            //     'headers': {
-            //         'content-Type': 'application/json',
-            //         'Authorization': 'Bearer 7da398b8-0b10-4b85-bb81-7e7a86cc2bfe0a8db05d-83fc-4f24-9f03-e685739592e0ac0eaa97-0356-469a-ba02-e7dd74ee81dc',
-            //     }
-            // };
-            // request(options, (err, res, body) => {
-            //     if (err) { return console.log(err); }
-            //     resolve(body.data);
-            //     console.log(body.data);
-            // });
+            // const data = fs.readFileSync('mock/accounts/get-accounts.json');
+            // const jsonData = JSON.parse(data.toString());
+            // resolve(jsonData.data);
+            const request = require('request');
+            const options = {
+                'method': 'GET',
+                'uri': 'https://oauth.bancsabadell.com/ResourcesServerBS/oauthservices/v1.0.0/productos',
+                'json': true,
+                'timeout': 2000,
+                'headers': {
+                    'content-Type': 'application/json',
+                    'Authorization': 'Bearer 7da398b8-0b10-4b85-bb81-7e7a86cc2bfe0a8db05d-83fc-4f24-9f03-e685739592e0ac0eaa97-0356-469a-ba02-e7dd74ee81dc',
+                }
+            };
+            request(options, (err, res, body) => {
+                if (err) {
+                    return console.log(err);
+                }
+                resolve(body.data);
+                console.log(body.data);
+            });
         });
     }
     getAccount(last4) {
