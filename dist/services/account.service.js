52c5fe200bec38f975cc2ce26fcd36daa9e49b96
pturalgi <pturalgi@everis.com>
2018-10-04 10:02:28 +0200

    account linking

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -9,14 +9,14 @@ class AccountService extends rest_manager_1.RestManager {
             // const data = fs.readFileSync('mock/accounts/get-accounts.json');
             // const jsonData = JSON.parse(data.toString());
             // resolve(jsonData.data);
-            const request = require('request');
+            const request = require('request-promise');
             const options = {
                 'method': 'GET',
                 'uri': 'https://oauth.bancsabadell.com/ResourcesServerBS/oauthservices/v1.0.0/productos',
                 'json': true,
                 'timeout': 8000,
                 'headers': {
-                    'content-Type': 'application/json',
+                    'Content-Type': 'application/json',
                     'Authorization': 'Bearer 7da398b8-0b10-4b85-bb81-7e7a86cc2bfe0a8db05d-83fc-4f24-9f03-e685739592e0ac0eaa97-0356-469a-ba02-e7dd74ee81dc',
                 }
             };
--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -11,7 +11,7 @@ export class AccountService extends RestManager {
                 // const jsonData = JSON.parse(data.toString());
                 // resolve(jsonData.data);
 
-            const request = require('request');
+            const request = require('request-promise');
 
             const options = {
                 'method': 'GET',
@@ -19,7 +19,7 @@ export class AccountService extends RestManager {
                 'json': true,
                 'timeout': 8000,
                 'headers': {
-                    'content-Type': 'application/json',
+                    'Content-Type': 'application/json',
                     'Authorization': 'Bearer 7da398b8-0b10-4b85-bb81-7e7a86cc2bfe0a8db05d-83fc-4f24-9f03-e685739592e0ac0eaa97-0356-469a-ba02-e7dd74ee81dc',
                 }
             };
