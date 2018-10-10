85076e5ed02abd3a271a73a414d487933ad2fb32
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-10-10 12:07:46 +0200

    new intent

--- dist/dialogflow/intents/accounts.js
+++ dist/dialogflow/intents/accounts.js
@@ -82,14 +82,15 @@ class AccountIntents /*extends BaseIntent*/ {
         // SALDO CUENTA
         app.intent('Saldo cuenta', (conv, input, { last4numbers }, { tipo_cuenta }) => {
             this.accountService.getAccount(last4numbers).then(account => {
-                const response = account_manager_2.AccountDFManager.saldoAccount(account);
-                conv.ask(response);
-                // if (account) {
-                //     conv.ask(`El saldo  de tu ${account.descripcion} es de ${account.balance} €`);
-                //     conv.ask(suggestionResponse);
-                // } else {
-                //     conv.ask(nullResponse);
-                // }
+                // const response = AccountDFManager.saldoAccount(account);
+                // conv.ask(response);
+                if (account) {
+                    conv.ask(`El saldo  de tu ${account.descripcion} es de ${account.balance} €`);
+                    conv.ask(suggestionResponse);
+                }
+                else {
+                    conv.ask(nullResponse);
+                }
             });
         });
     }
