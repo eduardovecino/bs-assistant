cad51fe7f007813c6b378b455eb16ef240896dce
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-10-10 12:19:37 +0200

    new intent

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -15,8 +15,8 @@ class AccountService extends rest_manager_1.RestManager {
         return new Promise((resolve, reject) => {
             const data = fs.readFileSync('mock/accounts/get-accounts.json');
             const jsonData = JSON.parse(data.toString());
-            const account = account_manager_1.AccountManager.getAccountByLast4(jsonData.data, last4);
-            resolve(account);
+            const accounts = account_manager_1.AccountManager.getAccountByLast4(jsonData.data, last4);
+            resolve(accounts);
         });
     }
     getMovementsAccounts() {
