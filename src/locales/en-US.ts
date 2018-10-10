bd6dc5ce45154c83efc76e0557674ffd8d25896e
pturalgi <pturalgi@everis.com>
2018-10-10 08:22:39 +0200

    account linking test 2

--- src/locales/en-US.ts
+++ src/locales/en-US.ts
@@ -1,4 +1,4 @@
 export const ENGLISH_TRANSLATIONS= {
-    'intent.product.welcome.answer': 'In order to forward you by your name and know your location,',
+    'intent.product.welcome.answer': 'In order to forward you by your name and know your location 2,',
     'intent.product.welcome.answer_%name%': 'Welcome to Banco Sabadell, {{ name }}'
 }
