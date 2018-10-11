3eaafac5cab56eb9a35df73bb9a95e5330e59634
pturalgi <pturalgi@everis.com>
2018-10-11 17:12:20 +0200

    services movements

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -24,7 +24,7 @@ export class AccountService extends RestManager {
         });
     }
 
-    public getMovementsAccounts(account): Promise<any> {
+    public getMovementsAccounts(account) {
         return this.getApiBSabadell(`/ResourcesServerBS/oauthservices/v1.0.0/cuentasvista/${account}/movimientos?fechaDesde=01-01-2016&fechaHasta=01-1-2018`, `mock/accounts/get-movements-accounts.json`);
         // return new Promise((resolve, reject) => {
         //     const data = fs.readFileSync('mock/accounts/get-movements-accounts.json');
