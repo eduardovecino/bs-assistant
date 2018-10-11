83a1b7a19fc1500173e36eff8fbf77e13e5046bb
eduardovecino <eduardo.vecino.gonzalez@everis.com>
2018-10-11 15:06:31 +0200

    commits

--- dist/dialogflow/intents/cards.js
+++ dist/dialogflow/intents/cards.js
@@ -41,6 +41,7 @@ class CardIntents /*extends BaseIntent*/ {
                 const lastNumbers = format_manager_1.FormatManager.getLast4numbers(cardSelected.cuentaRelacionada);
                 conv.contexts.set(Contexts.selected_card, 5);
                 conv.contexts.delete(Contexts.selected_account, 5);
+                console.log(Contexts.selected_account);
                 if (cardSelected) {
                     conv.ask(ssml_gib_1.Ssml.wrapSsmlSpeak([`Has seleccionado la tarjeta finalizada en ${cardSelected.cuentaRelacionada}, el saldo es de ${cardSelected.saldoDisponible} €. ${ssml_gib_1.Ssml.break({ s: 3 })} ¿Quieres saber algo más a cerca de tus tarjetas?`]));
                 }
