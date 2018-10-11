db05f0afb3b7f7f2aa2c5bfb0eb2127403fab5a8
pturalgi <pturalgi@everis.com>
2018-10-11 16:08:12 +0200

    services movements

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -2,7 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const rest_manager_1 = require("../managers/data/rest.manager");
 const account_manager_1 = require("../managers/data/account.manager");
-const fs = require("fs");
 class AccountService extends rest_manager_1.RestManager {
     getAccounts() {
         return this.getApiBSabadell('/ResourcesServerBS/oauthservices/v1.0.0/productos', 'mock/accounts/get-accounts.json');
@@ -14,7 +13,9 @@ class AccountService extends rest_manager_1.RestManager {
     }
     getAccount(last4) {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/accounts/get-accounts.json');
+            // const data = fs.readFileSync('mock/accounts/get-accounts.json');
+            let data;
+            data = this.getApiBSabadell('/ResourcesServerBS/oauthservices/v1.0.0/productos', 'mock/accounts/get-accounts.json');
             const jsonData = JSON.parse(data.toString());
             const account = account_manager_1.AccountManager.getAccountByLast4(jsonData.data, last4);
             resolve(account);
--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -18,7 +18,9 @@ export class AccountService extends RestManager {
 
     public getAccount(last4): Promise<any> {
         return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/accounts/get-accounts.json');
+            // const data = fs.readFileSync('mock/accounts/get-accounts.json');
+            let data;
+            data = this.getApiBSabadell('/ResourcesServerBS/oauthservices/v1.0.0/productos', 'mock/accounts/get-accounts.json');
             const jsonData = JSON.parse(data.toString());
             const account = AccountManager.getAccountByLast4(jsonData.data, last4);
             resolve(account);
