7a61b464c60d705edffd4ccb130c433b44d6fa00
pturalgi <pturalgi@everis.com>
2018-10-11 16:46:09 +0200

    services movements

--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -17,14 +17,10 @@ export class AccountService extends RestManager {
     }
 
     public getAccount(last4) {
-        // return new Promise((resolve, reject) => {
-            // const data = fs.readFileSync('mock/accounts/get-accounts.json');
-            this.getApiBSabadell('/ResourcesServerBS/oauthservices/v1.0.0/productos', 'mock/accounts/get-accounts.json').then(accounts => {
-                const jsonData = JSON.parse(accounts.toString());
-                const account = AccountManager.getAccountByLast4(jsonData.data, last4);
-                return account;
-            })
-        // });
+        const accounts = this.getAccounts();
+        const jsonData = JSON.parse(accounts.toString());
+        const account = AccountManager.getAccountByLast4(jsonData.data, last4);
+        return account;
     }
 
     public getMovementsAccounts(account): Promise<any> {
