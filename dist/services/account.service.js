f3c629ba206dcc7d911b3a192bc83b0e1e7c6406
pturalgi <pturalgi@everis.com>
2018-10-15 17:26:30 +0200

    services movements

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -14,9 +14,9 @@ class AccountService extends rest_manager_1.RestManager {
     getAccount(last4) {
         console.log("ACCOUNT.SERVICE " + last4);
         this.getAccounts().then(accounts => {
-            const jsonData = JSON.parse(accounts.toString());
-            console.log("ACCOUNT.SERVICE2 " + JSON.stringify(jsonData));
-            const account = account_manager_1.AccountManager.getAccountByLast4(jsonData.data, last4);
+            // const jsonData = JSON.parse(accounts.toString());
+            console.log("ACCOUNT.SERVICE2 " + JSON.stringify(accounts));
+            const account = account_manager_1.AccountManager.getAccountByLast4(accounts, last4);
             console.log("987654321 " + account);
             return account;
         });
--- src/services/account.service.ts
+++ src/services/account.service.ts
@@ -19,9 +19,9 @@ export class AccountService extends RestManager {
     public getAccount(last4) {
         console.log("ACCOUNT.SERVICE " +last4);
         this.getAccounts().then(accounts=> {
-            const jsonData = JSON.parse(accounts.toString());
-            console.log("ACCOUNT.SERVICE2 " + JSON.stringify(jsonData));
-            const account = AccountManager.getAccountByLast4(jsonData.data, last4);
+            // const jsonData = JSON.parse(accounts.toString());
+            console.log("ACCOUNT.SERVICE2 " + JSON.stringify(accounts));
+            const account = AccountManager.getAccountByLast4(accounts, last4);
             console.log("987654321 "+account);
             return account;
         });
