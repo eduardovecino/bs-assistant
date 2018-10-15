d243c0b633b8bb02cf0855137e8d2f414cfdd6d6
pturalgi <pturalgi@everis.com>
2018-10-15 16:47:54 +0200

    services movements

--- dist/dialogflow/intents/accounts.js
+++ dist/dialogflow/intents/accounts.js
@@ -85,8 +85,11 @@ class AccountIntents /*extends BaseIntent*/ {
         }));
         // SALDO CUENTA
         app.intent('Saldo cuenta', (conv, { last4numbers }, { tipo_cuenta }) => __awaiter(this, void 0, void 0, function* () {
+            console.log("SALDOCUENTA1");
             let account = yield this.accountService.getAccount(last4numbers);
+            console.log("SALDOCUENTA2 " + account);
             const response = account_manager_2.AccountDFManager.saldoAccount(account);
+            console.log("SALDOCUENTA3 " + response);
             conv.ask(response);
         }));
         //MOVIMIENTOS CUENTA
--- src/dialogflow/intents/accounts.ts
+++ src/dialogflow/intents/accounts.ts
@@ -85,8 +85,11 @@ export class AccountIntents /*extends BaseIntent*/ {
         
         // SALDO CUENTA
         app.intent('Saldo cuenta', async (conv, { last4numbers }, { tipo_cuenta }) => {
+            console.log("SALDOCUENTA1");
             let account = await this.accountService.getAccount(last4numbers);
+            console.log("SALDOCUENTA2 " + account);
             const response = AccountDFManager.saldoAccount(account);
+            console.log("SALDOCUENTA3 " + response);
             conv.ask(response);
         });
         
