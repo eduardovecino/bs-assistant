b56f50e230363c3e4736a5acef9f85f48c4895e9
pturalgi <pturalgi@everis.com>
2018-10-15 17:16:32 +0200

    services movements

--- dist/services/account.service.js
+++ dist/services/account.service.js
@@ -14,8 +14,8 @@ class AccountService extends rest_manager_1.RestManager {
     getAccount(last4) {
         console.log("ACCOUNT.SERVICE " + last4);
         this.getAccounts().then(accounts => {
-            const jsonData = JSON.parse(JSON.stringify(accounts));
-            console.log("ACCOUNT.SERVICE2 " + jsonData);
+            const jsonData = JSON.parse(accounts.toString());
+            console.log("ACCOUNT.SERVICE2 " + JSON.stringify(jsonData.data));
             const account = account_manager_1.AccountManager.getAccountByLast4(jsonData.data, last4);
             console.log("987654321 " + account);
             return account;
