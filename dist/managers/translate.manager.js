2095d1e02aaafc0cbfbb04b4c4a8297f068ae341
pturalgi <pturalgi@everis.com>
2018-10-17 13:28:15 +0200

    services movements

--- dist/managers/translate.manager.js
+++ dist/managers/translate.manager.js
@@ -17,17 +17,12 @@ class TranslateManager {
     }
     translate(key, params) {
         let literal = this._config.translations[this._config.lang][key];
-        console.log("liiteral1" + literal);
-        let startCharacter = literal.indexOf('{');
-        console.log("liiteral2" + startCharacter);
-        let endCharacter = literal.lastIndexOf('}');
-        console.log("liiteral3" + endCharacter);
-        // let startPart = literal.split('{{');
-        // let endPart = startPart[1].split('}}');
-        let selection = literal.slice(startCharacter - 1, endCharacter + 1);
-        console.log("liiteral4" + selection);
-        literal = literal.replace(selection, params);
-        console.log("liiteral5" + literal);
+        if (params) {
+            let startCharacter = literal.indexOf('{');
+            let endCharacter = literal.lastIndexOf('}');
+            let selection = literal.slice(startCharacter - 1, endCharacter + 1);
+            literal = literal.replace(selection, params);
+        }
         return literal;
     }
 }
