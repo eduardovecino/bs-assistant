771063599acb33ef9f5441965b838b7864c761a9
pturalgi <pturalgi@everis.com>
2018-10-17 16:45:55 +0200

    services movements

--- dist/dialogflow/intents/accounts.js
+++ dist/dialogflow/intents/accounts.js
@@ -22,8 +22,8 @@ class AccountIntents /*extends BaseIntent*/ {
     intents(app) {
         const nullResponse = 'No se ha encontrado ninguna cuenta, prueba en decir el tipo de cuenta o los 4 últimos numeros';
         // const nullResponse = this.translateManager.translate('intent.account.null_response');
-        const suggestionResponse = 'Puedes preguntarme por el saldo o los movimientos de una cuenta';
-        // const suggestionResponse = this.translateManager.translate('intent.account.suggestion_response');
+        // const suggestionResponse = 'Puedes preguntarme por el saldo o los movimientos de una cuenta';
+        const suggestionResponse = this.translateManager.translate('intent.account.suggestion_response');
         const Contexts = {
             selected_account: 'selected_account',
             selected_card: 'selected_card'
