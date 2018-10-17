5812103ba4bafbd97c7d03cd9fb9f36cf6b04886
pturalgi <pturalgi@everis.com>
2018-10-17 10:11:03 +0200

    services movements

--- dist/dialogflow/intents/info.js
+++ dist/dialogflow/intents/info.js
@@ -1,29 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const information_service_1 = require("../../services/information.service");
-const information_manager_1 = require("../../managers/dialog-flow/information.manager");
 class InfoIntents /*extends BaseIntent*/ {
     constructor() {
         this.informationService = new information_service_1.InformationService();
     }
     intents(app) {
-        app.intent('Oficinas Cercanas', conv => {
-            this.informationService.getOffices().then(offices => {
-                let response = "Tienes " + offices.length + " oficinas cercanas a tu posición. ";
-                // conv.ask(offices[0].latitude);
-                if (offices) {
-                    offices.forEach(office => {
-                        response = response + office.id + " en " + office.address + ", ";
-                    });
-                    const carouselOfOffices = information_manager_1.InformationDFManager.generateOfficesBrowseCarousel(offices);
-                    conv.ask(response + "¿Cúal quieres seleccionar?");
-                    conv.ask(carouselOfOffices);
-                }
-                else {
-                    conv.ask(`No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo`);
-                }
-            });
-        });
+        app.intent('Oficinas Cercanas', (conv) => __awaiter(this, void 0, void 0, function* () {
+            let offices;
+            offices = yield this.informationService.getOffices();
+            if (offices) {
+                console.log("Localizaciones cajeros" + JSON.stringify(offices));
+                conv.ask("Localizaciones cajeros" + JSON.stringify(offices));
+            }
+            // let response = "Tienes " + offices.length + " oficinas cercanas a tu posición. ";
+            // if (offices) {
+            //     offices.forEach(office => {
+            //         response = response + office.id + " en " + office.address + ", ";
+            //     })
+            //     const carouselOfOffices = InformationDFManager.generateOfficesBrowseCarousel(offices);
+            //     conv.ask(response + "¿Cúal quieres seleccionar?");
+            //     conv.ask(carouselOfOffices);
+            // } else {
+            //     conv.ask(`No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo`);
+            // }
+        }));
     }
 }
 exports.InfoIntents = InfoIntents;
--- src/dialogflow/intents/info.ts
+++ src/dialogflow/intents/info.ts
@@ -9,23 +9,26 @@ export class InfoIntents /*extends BaseIntent*/ {
 
     public intents(app): void {
 
-        app.intent('Oficinas Cercanas', conv => {
-            this.informationService.getOffices().then(offices => {
-                let response = "Tienes " + offices.length + " oficinas cercanas a tu posición. ";
+        app.intent('Oficinas Cercanas', async conv => {
+            let offices;
+            offices = await this.informationService.getOffices();
+            if (offices){
+                console.log("Localizaciones cajeros" + JSON.stringify(offices));
+                conv.ask("Localizaciones cajeros" + JSON.stringify(offices));
+            }
 
-                // conv.ask(offices[0].latitude);
-                if (offices) {
-                    offices.forEach(office => {
-                        response = response + office.id + " en " + office.address + ", ";
-                    })
+            // let response = "Tienes " + offices.length + " oficinas cercanas a tu posición. ";
+            // if (offices) {
+            //     offices.forEach(office => {
+            //         response = response + office.id + " en " + office.address + ", ";
+            //     })
 
-                    const carouselOfOffices = InformationDFManager.generateOfficesBrowseCarousel(offices);
-                    conv.ask(response + "¿Cúal quieres seleccionar?");
-                    conv.ask(carouselOfOffices);
-                } else {
-                    conv.ask(`No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo`);
-                }
-            });
+            //     const carouselOfOffices = InformationDFManager.generateOfficesBrowseCarousel(offices);
+            //     conv.ask(response + "¿Cúal quieres seleccionar?");
+            //     conv.ask(carouselOfOffices);
+            // } else {
+            //     conv.ask(`No podemos mostrar las oficinas cercanas en este momento, vuelve a intentarlo`);
+            // }
         });
     }
 }
