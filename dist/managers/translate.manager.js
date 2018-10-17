ee8b9464a2ae22b4d266c1644a22a109b4f39447
pturalgi <pturalgi@everis.com>
2018-10-17 13:13:50 +0200

    services movements

--- dist/managers/translate.manager.js
+++ dist/managers/translate.manager.js
@@ -17,13 +17,17 @@ class TranslateManager {
     }
     translate(key, params) {
         let literal = this._config.translations[this._config.lang][key];
+        console.log("liiteral1" + literal);
         let startCharacter = literal.indexOf('{');
+        console.log("liiteral2" + startCharacter);
         let endCharacter = literal.lastIndexOf('{');
+        console.log("liiteral3" + endCharacter);
         // let startPart = literal.split('{{');
         // let endPart = startPart[1].split('}}');
         let selection = literal.slice(startCharacter - 1, endCharacter - 1);
+        console.log("liiteral4" + selection);
         literal = literal.replace(selection, params);
-        console.log("liiteral" + literal);
+        console.log("liiteral5" + literal);
         return literal;
     }
 }
--- src/managers/translate.manager.ts
+++ src/managers/translate.manager.ts
@@ -25,14 +25,21 @@ export class TranslateManager {
 
     public translate(key, params?) {
         let literal = this._config.translations[this._config.lang][key];
+        console.log("liiteral1" + literal);
+
         let startCharacter = literal.indexOf('{');
+        console.log("liiteral2" + startCharacter);
+
         let endCharacter = literal.lastIndexOf('{');
+        console.log("liiteral3" + endCharacter);
         
         // let startPart = literal.split('{{');
         // let endPart = startPart[1].split('}}');
         let selection = literal.slice(startCharacter-1, endCharacter-1);
+        console.log("liiteral4" + selection);
+
         literal = literal.replace(selection , params);
-        console.log("liiteral" + literal);
+        console.log("liiteral5" + literal);
         return literal;
     }
 }
