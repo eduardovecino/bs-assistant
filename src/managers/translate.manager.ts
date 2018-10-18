70ca170f3eb87dbd504d14afea524353f7feb5d7
pturalgi <pturalgi@everis.com>
2018-10-18 17:01:33 +0200

    services movements

--- src/managers/translate.manager.ts
+++ src/managers/translate.manager.ts
@@ -26,9 +26,9 @@ export class TranslateManager {
     public translate(key, params?) {
         let literal = this._config.translations[this._config.lang][key];
         if (params){
-            for(let i=0; i<params.lenght+1; i++){
-                literal = literal.replace(/\{{.*?\}}/, params[i]);
-            }
+            // for(let i=0; i<params.lenght+1; i++){
+                literal = literal.replace(/\{{.*?\}}/, 'caca');
+            // }
             // let startCharacter = literal.indexOf('{');
             // let endCharacter = literal.lastIndexOf('}');
             // let selection = literal.slice(startCharacter - 1, endCharacter + 1);
