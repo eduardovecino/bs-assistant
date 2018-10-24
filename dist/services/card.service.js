7c1b4e3a16b179a5738f6833d7a260ede49a87f8
pturalgi <pturalgi@everis.com>
2018-10-24 09:13:49 +0200

    services movements

--- dist/services/card.service.js
+++ dist/services/card.service.js
@@ -5,39 +5,37 @@ const card_manager_1 = require("../managers/data/card.manager");
 const card_model_1 = require("../models/card.model");
 const fs = require("fs");
 class CardService extends rest_manager_1.RestManager {
-    getCards2() {
+    // public getCards2(): Promise<any> {
+    //     return new Promise((resolve, reject) => {
+    //         const data = fs.readFileSync('mock/card/get-cards.json');
+    //         const jsonData = JSON.parse(data.toString());
+    //         console.log("CARDS", jsonData);
+    //         resolve(jsonData.data);
+    //     });      
+    // }
+    // public getCard(last4): Promise<any> {
+    //     return new Promise((resolve, reject) => {
+    //         const data = fs.readFileSync('mock/card/get-cards.json');
+    //         const jsonData = JSON.parse(data.toString());
+    //         const card = CardManager.getCardByLast4(jsonData.data, last4);
+    //         resolve(card);
+    //     });
+    // }
+    getCards() {
         return new Promise((resolve, reject) => {
             const data = fs.readFileSync('mock/card/get-cards.json');
             const jsonData = JSON.parse(data.toString());
-            console.log("CARDS", jsonData);
-            resolve(jsonData.data);
+            const cards = [];
+            jsonData.data.forEach(card => cards.push(new card_model_1.CardModel(card)));
+            resolve(cards);
         });
     }
     getCard(last4) {
         return new Promise((resolve, reject) => {
             const data = fs.readFileSync('mock/card/get-cards.json');
             const jsonData = JSON.parse(data.toString());
-            const card = card_manager_1.CardManager.getCardByLast4(jsonData.data, last4);
-            resolve(card);
-        });
-    }
-    getCards() {
-        return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/card/get-cards.json');
-            const jsonData = JSON.parse(data.toString());
-            console.log("CARDS0", jsonData.data);
-            const cards = [];
-            jsonData.data.forEach(card => cards.push(new card_model_1.CardModel(card)));
-            console.log("CARDS1", cards);
-            resolve(cards);
-        });
-    }
-    getCard2(last4) {
-        return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/card/get-cards.json');
-            const jsonData = JSON.parse(data.toString());
             const card = new card_model_1.CardModel(card_manager_1.CardManager.getCardByLast4(jsonData.data, last4));
-            console.log("CARD", card);
+            console.log("CARD0", card);
             resolve(card);
         });
     }
--- src/services/card.service.ts
+++ src/services/card.service.ts
@@ -6,42 +6,40 @@ import * as fs from "fs";
 
 export class CardService extends RestManager {
 
-    public getCards2(): Promise<any> {
+    // public getCards2(): Promise<any> {
+    //     return new Promise((resolve, reject) => {
+    //         const data = fs.readFileSync('mock/card/get-cards.json');
+    //         const jsonData = JSON.parse(data.toString());
+    //         console.log("CARDS", jsonData);
+    //         resolve(jsonData.data);
+    //     });      
+    // }
+
+    // public getCard(last4): Promise<any> {
+    //     return new Promise((resolve, reject) => {
+    //         const data = fs.readFileSync('mock/card/get-cards.json');
+    //         const jsonData = JSON.parse(data.toString());
+    //         const card = CardManager.getCardByLast4(jsonData.data, last4);
+    //         resolve(card);
+    //     });
+    // }
+
+    public getCards(): Promise<any> {
         return new Promise((resolve, reject) => {
             const data = fs.readFileSync('mock/card/get-cards.json');
             const jsonData = JSON.parse(data.toString());
-            console.log("CARDS", jsonData);
-            resolve(jsonData.data);
-        });      
+            const cards: Array<CardModel> = [];
+            jsonData.data.forEach(card => cards.push(new CardModel(card)));
+            resolve(cards);
+        });
     }
 
     public getCard(last4): Promise<any> {
         return new Promise((resolve, reject) => {
             const data = fs.readFileSync('mock/card/get-cards.json');
             const jsonData = JSON.parse(data.toString());
-            const card = CardManager.getCardByLast4(jsonData.data, last4);
-            resolve(card);
-        });
-    }
-
-    public getCards(): Promise<any> {
-        return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/card/get-cards.json');
-            const jsonData = JSON.parse(data.toString());
-            console.log("CARDS0", jsonData.data);
-            const cards: Array<CardModel> = [];
-            jsonData.data.forEach(card => cards.push(new CardModel(card)));
-            console.log("CARDS1", cards);
-            resolve(cards);
-        });
-    }
-
-    public getCard2(last4): Promise<any> {
-        return new Promise((resolve, reject) => {
-            const data = fs.readFileSync('mock/card/get-cards.json');
-            const jsonData = JSON.parse(data.toString());
             const card: CardModel = new CardModel(CardManager.getCardByLast4(jsonData.data, last4));
-            console.log("CARD", card);
+            console.log("CARD0", card);
             resolve(card);
         });
     }
