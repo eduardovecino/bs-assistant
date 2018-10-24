57d8120f91a310b418b1b53ef941ca247fa9ef4a
pturalgi <pturalgi@everis.com>
2018-10-24 10:51:24 +0200

    services movements

--- src/managers/dialog-flow/card.manager.ts
+++ src/managers/dialog-flow/card.manager.ts
@@ -84,9 +84,12 @@ export class CardDFManager {
         let response = ' ';
         let length = (movements.length > 3) ? 3 : movements.length + 1;
         if (movements) {
+            console.log("ENTROOOO");
             for (let i = 0; i < length; i++) {
                 response = response + this.translateManager.translate('intent.card.movements.simple_response.pre_%concept%_%import%', [movements[i].concept, movements[i].amount])
+                console.log("ENTROOOO", response);
             };
+            console.log("SURTOOOOO");
             return this.translateManager.translate('intent.card.movements.simple_response_%number%_%movements%', [movements.length, response]);
         } else {
             return this.translateManager.translate('intent.card.movements.no_movements');
