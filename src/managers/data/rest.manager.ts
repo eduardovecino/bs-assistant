02690eb56e5b10e5beaac0c4edafd582af2f1c4f
pturalgi <pturalgi@everis.com>
2018-11-05 17:29:14 +0100

    new token

--- src/managers/data/rest.manager.ts
+++ src/managers/data/rest.manager.ts
@@ -1,8 +1,8 @@
 import * as fs from "fs";
 import * as rp from "request-promise";
 
-const host = 'https://oauth.bancsabadell.com';
-const token = '87dfdac5-7ce4-4ee8-b2ab-ae621650266230493354-4ddf-4c5e-88fd-0171f55ff301f3190023-879d-4fc7-9a84-effe1775c79d'
+const host = 'https://developers.bancsabadell.com';
+const token = 'f73db91b-2713-4493-a6a4-3b1946b0f05b49830d1f-cc65-4ccf-8b7a-d235b1b07d3333a89894-d172-4b0f-826a-5267cf7520ee'
 
 export class RestManager {
 
