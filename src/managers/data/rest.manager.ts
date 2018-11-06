9f6a47f3062975f9b1b3776147a913011825cfd6
pturalgi <pturalgi@everis.com>
2018-11-06 15:19:15 +0100

    new token

--- src/managers/data/rest.manager.ts
+++ src/managers/data/rest.manager.ts
@@ -5,7 +5,7 @@ import * as rp from "request-promise";
 // const token = '87dfdac5-7ce4-4ee8-b2ab-ae621650266230493354-4ddf-4c5e-88fd-0171f55ff301f3190023-879d-4fc7-9a84-effe1775c79d'
 
 const host = 'https://developers.bancsabadell.com';
-const token = 'ab1f09c5-5459-4f7a-92a4-4861b8bdfe76469e8167-cc03-4f09-88c0-425a55ac9c9bdef4f127-0e6f-45bf-a396-591c3c9842e5'
+const token = '6c2edddc-d110-487b-9267-92c6e69a5c9460c0aaf6-15f6-4b93-b9a4-efcf2701848fcbbbdbde-bc19-4625-aa8c-d5adfeb36c37'
 
 export class RestManager {
 
@@ -28,7 +28,6 @@ export class RestManager {
                 },
                 'strictSSL': false
             };
-
             if (this.isMock) {
                 const data = fs.readFileSync(mock);
                 const jsonData = JSON.parse(data.toString());
