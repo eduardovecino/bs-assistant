814bd258d80fd036042f421979b0bf733e1d0212
pturalgi <pturalgi@everis.com>
2018-11-06 17:20:48 +0100

    new token

--- dist/models/cards.model.js
+++ dist/models/cards.model.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const format_manager_1 = require("../managers/format.manager");
 class CardsModel {
     constructor(data) {
         this.fromJSON(data);
@@ -47,9 +46,9 @@ class CardsModel {
     set numberCodificatedProduct(value) {
         this._numberCodificatedProduct = value;
     }
-    get last4Numbers() {
-        return format_manager_1.FormatManager.getLast4numbers(this.relatedAccount);
-    }
+    // get last4Numbers(): string {
+    //     return FormatManager.getLast4numbers(this.relatedAccount);
+    // }
     fromJSON(data) {
         this._owner = data.propietario;
         this._description = data.descripcion;
