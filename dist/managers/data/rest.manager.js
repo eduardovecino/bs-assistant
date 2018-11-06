a12d27473c3ed139744ff3b267dd605fdd7e7d2e
pturalgi <pturalgi@everis.com>
2018-11-06 09:20:13 +0100

    new token

--- dist/managers/data/rest.manager.js
+++ dist/managers/data/rest.manager.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const rp = require("request-promise");
-const host = 'https://developers.bancsabadell.com';
-const token = '6b4cdff4-37f2-4772-83d4-98b6e80d75950b85ef2e-f36d-445d-94f5-1438446ce1b38fd2635a-06ca-46f2-9c38-52399bcce257';
+const host = 'https://oauth.bancsabadell.com';
+const token = '87dfdac5-7ce4-4ee8-b2ab-ae621650266230493354-4ddf-4c5e-88fd-0171f55ff301f3190023-879d-4fc7-9a84-effe1775c79d';
 class RestManager {
     constructor() {
         this.isMock = process.env.MOCK;
